f9c9537acd2d93dcd1d840b6cab6316f0ee91d29
kaitlynishida <kais0261@colorado.edu>
2021-10-29 03:06:47 -0600

    create index.html
    main page of midterm

---
+++ midterm2/index.js
@@ -0,0 +1,29 @@
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let images = ["billie.jpg", "cage.jpg", "sza.jpg", "strokes.jpg", "doja.jpg", "rihanna.jpg", "tyler.jpg"];
+let prevday;
+
+for(i=0; i<document.getElementsByClassName("days").length; i++){
+  document.getElementsByClassName("days")[i].innerHTML = "<h2>" + days[i] + "</h2>";
+  document.getElementsByClassName("days")[i].addEventListener('click', setActive, false);
+  document.getElementsByClassName("days")[i].id = days[i];
+}
+
+document.getElementById('Sunday').classList.toggle('active');
+document.getElementById("music").innerHTML = "<img src='assets/" + images[0] + "' >";
+
+function setActive(e){
+  prevday = document.getElementsByClassName("active")[0].id;
+  if(prevday != undefined){
+    document.getElementById(prevday).classList.toggle("active");
+  }
+
+  if(e.target.tagName == "H2"){
+    e.target.parentNode.classList.toggle("active");
+  }else{
+    e.target.classList.toggle("active");
+  }
+
+  day = document.getElementsByClassName('active')[0].id;
+  console.log(day);
+  document.getElementById('music').innerHTML = "<img src='assets/" + day + ".jpg' >";
+}
